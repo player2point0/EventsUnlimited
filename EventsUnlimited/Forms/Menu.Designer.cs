0b30b335f4e387b6fe3603422eb74a5e2d77d625
nathan <nathannash11@gmail.com>
2017-12-14 15:10:55 +0000

    Added a button to menu and changed properties

--- EventsUnlimited/Forms/Menu.Designer.cs
+++ EventsUnlimited/Forms/Menu.Designer.cs
@@ -28,20 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.BtnExit = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnExit.Location = new System.Drawing.Point(402, 460);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(239, 167);
+            this.BtnExit.TabIndex = 0;
+            this.BtnExit.Text = "Exit";
+            this.BtnExit.UseVisualStyleBackColor = true;
+            this.BtnExit.Click += new System.EventHandler(this.button1_Click);
             // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(278, 244);
+            this.ClientSize = new System.Drawing.Size(1082, 678);
+            this.ControlBox = false;
+            this.Controls.Add(this.BtnExit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "Menu";
-            this.Text = "Menu";
+            this.Text = "Events Unlimited";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Menu_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnExit;
     }
 }
 
--- EventsUnlimited/Forms/Menu.cs
+++ EventsUnlimited/Forms/Menu.cs
@@ -16,5 +16,15 @@ namespace EventsUnlimited
         {
             InitializeComponent();
         }
+
+        private void Menu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
