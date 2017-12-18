062cde0d195701351b9793408b0e64c01e077438
nathan <nathannash11@gmail.com>
2017-12-18 10:47:29 +0000

    Added logo to menu form

--- EventsUnlimited/Forms/Menu.Designer.cs
+++ EventsUnlimited/Forms/Menu.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.BtnExit = new System.Windows.Forms.Button();
+            this.PnlCustomerActions = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // BtnExit
@@ -44,18 +47,39 @@
             this.BtnExit.UseVisualStyleBackColor = true;
             this.BtnExit.Click += new System.EventHandler(this.button1_Click);
             // 
+            // PnlCustomerActions
+            // 
+            this.PnlCustomerActions.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PnlCustomerActions.Location = new System.Drawing.Point(103, 226);
+            this.PnlCustomerActions.Name = "PnlCustomerActions";
+            this.PnlCustomerActions.Size = new System.Drawing.Size(239, 308);
+            this.PnlCustomerActions.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::EventsUnlimited.Properties.Resources.Logo;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(215, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(713, 155);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1082, 678);
             this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.PnlCustomerActions);
             this.Controls.Add(this.BtnExit);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "Menu";
             this.Text = "Events Unlimited";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Menu_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -63,6 +87,8 @@
         #endregion
 
         private System.Windows.Forms.Button BtnExit;
+        private System.Windows.Forms.Panel PnlCustomerActions;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
