80b2154982c78d7c1d4d44f41c8e9d6bca09afd2
nathan <nathannash11@gmail.com>
2018-01-22 09:25:34 +0000

    Hooked up the staff form to SQL

--- EventsUnlimited/Forms/Template/Staff.cs
+++ EventsUnlimited/Forms/Template/Staff.cs
@@ -12,6 +12,10 @@ namespace EventsUnlimited
 {
     public partial class FrmStaff : FrmTemplate
     {
+        private SQLManager sqlManager;
+        private int index;
+        private Control[] controls;
+
         public FrmStaff()
         {
             InitializeComponent();
@@ -19,9 +23,19 @@ namespace EventsUnlimited
 
         private void FrmStaff_Load(object sender, EventArgs e)
         {
+            string name = "Staff";
+            string[] primaryKeys = new string[] { "StaffId"};
+            string[] fields = new string[] { "StaffId", "StaffName", "StaffAddress", "StaffPhoneNumber"};
 
+            sqlManager = new SQLManager(name, primaryKeys, fields);
+
+            sqlManager.ReadTable();
+
+            index = 0;
+            controls = new Control[] {LblStaffID, TbxStaffName, TbxStaffAddress, TbxStaffPhoneNumber };
+
+            sqlManager.ShowTable(ref index, ref controls);
         }
-
 
         protected override void BtnEdit_Click(object sender, EventArgs e)
         {
@@ -45,11 +59,16 @@ namespace EventsUnlimited
 
         protected override void BtnNext_Click(object sender, EventArgs e)
         {
+            index++;
+            string message = sqlManager.ShowTable(ref index, ref controls);
+            Print(message);
         }
 
         protected override void BtnPrevious_Click(object sender, EventArgs e)
         {
+            index--;
+            string message = sqlManager.ShowTable(ref index, ref controls);
+            Print(message);
         }
-
     }
 }
