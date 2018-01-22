807a47e42c9eb89ea6668e252b1df1bc95efb779
nathan <nathannash11@gmail.com>
2018-01-22 09:37:11 +0000

    Hooked up the supplier form to SQL

--- EventsUnlimited/Forms/Template/Supplier.cs
+++ EventsUnlimited/Forms/Template/Supplier.cs
@@ -12,6 +12,10 @@ namespace EventsUnlimited
 {
     public partial class FrmSupplier : FrmTemplate
     {
+        private SQLManager sqlManager;
+        private int index;
+        private Control[] controls;
+
         public FrmSupplier()
         {
             InitializeComponent();
@@ -19,9 +23,19 @@ namespace EventsUnlimited
 
         private void FrmSupplier_Load(object sender, EventArgs e)
         {
+            string name = "Supplier";
+            string[] primaryKeys = new string[] { "SupplierId"};
+            string[] fields = new string[] { "SupplierId", "SupplierName", "SupplierAddress", "SupplierPhoneNumber" };
 
+            sqlManager = new SQLManager(name, primaryKeys, fields);
+
+            sqlManager.ReadTable();
+
+            index = 0;
+            controls = new Control[] {LblSupplierID, TbxSupplierName, TbxSupplierAddress, TbxSupplierPhoneNumber };
+
+            sqlManager.ShowTable(ref index, ref controls);
         }
-
 
         protected override void BtnEdit_Click(object sender, EventArgs e)
         {
@@ -45,11 +59,16 @@ namespace EventsUnlimited
 
         protected override void BtnNext_Click(object sender, EventArgs e)
         {
+            index++;
+            string message = sqlManager.ShowTable(ref index, ref controls);
+            Print(message);
         }
 
         protected override void BtnPrevious_Click(object sender, EventArgs e)
         {
+            index--;
+            string message = sqlManager.ShowTable(ref index, ref controls);
+            Print(message);
         }
-
     }
 }
